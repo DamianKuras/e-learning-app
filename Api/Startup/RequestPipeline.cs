dd53841dfadecbfc5c80a692cd868975387003a4
DamianKuras <damian_kuras@outlook.com>
2022-12-01 22:50:56 +0100

    refactor: refactor Program.cs

---
+++ Api/Startup/RequestPipeline.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+
+namespace Api.Startup
+{
+    public static class RequestPipeline
+    {
+        public static WebApplication ConfigureRequestPipeline(this WebApplication app)
+        {
+            ConfigureSwagger(app);
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+            return app;
+        }
+
+
+        private static WebApplication ConfigureSwagger(WebApplication app)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(options =>
+                {
+                    var provider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+                    foreach (var description in provider.ApiVersionDescriptions)
+                    {
+                        options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json",
+                            description.ApiVersion.ToString());
+                    }
+                });
+            }
+            return app;
+        }
+    }
+}
---
+++ Api/Startup/Services.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using Api.Options;
+
+namespace Api.Startup
+{
+    public static class Services
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.ConfigureOptions<ConfigureSwaggerOptions>();
+            AddApiVersioning(services);
+            AddVersionedApiExplored(services);
+            return services;
+        }
+        private static void AddApiVersioning(IServiceCollection services)
+        {
+            services.AddApiVersioning(config =>
+            {
+                config.DefaultApiVersion = new ApiVersion(1, 0);
+                config.AssumeDefaultVersionWhenUnspecified = true;
+                config.ReportApiVersions = true;
+                config.ApiVersionReader = new UrlSegmentApiVersionReader();
+            });
+        }
+
+        private static void AddVersionedApiExplored(IServiceCollection services)
+        {
+            services.AddVersionedApiExplorer(config =>
+            {
+                config.GroupNameFormat = "'v'VVV";
+                config.SubstituteApiVersionInUrl = true;
+            });
+        }
+
+    }
+}
