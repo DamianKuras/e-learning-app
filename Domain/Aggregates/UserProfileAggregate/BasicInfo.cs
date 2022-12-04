475a6d2b66b3f78ea85f203f9389ad0716c5eca3
DamianKuras <damian_kuras@outlook.com>
2022-12-04 23:05:10 +0100

    feat: add user profile aggregate

---
+++ Domain/Aggregates/UserProfileAggregate/BasicInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Aggregates.UserProfileAggregate
+{
+    public class BasicInfo
+    {
+        private BasicInfo()
+        {
+        }
+        public string FirstName { get; private set; }
+        public string LastName { get;private set; }
+        public string EmailAddress { get;private set; }  
+
+
+        public static BasicInfo CreateBasicInfo(string firstName,string lastName, string emailAddress,
+            DateTime dateOfBirth)
+        {
+            BasicInfo basicInfo = new BasicInfo
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                EmailAddress = emailAddress,
+            };
+            return basicInfo;
+        }
+
+
+    }
+}
---
+++ Domain/Aggregates/UserProfileAggregate/UserProfile.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Aggregates.UserProfileAggregate
+{
+    public class UserProfile
+    {
+        private UserProfile()
+        {
+        }
+        public Guid Id { get; private set; }
+        public string IdentityId { get; private set; }
+
+        public BasicInfo BasicInfo { get; private set; }
+        public DateTime Created { get; private set; }
+        public DateTime LastUpdated { get; private set; }
+        public static UserProfile CreateUserProfile(string identityId, BasicInfo basicInfo)
+        {
+            var UserProfile = new UserProfile
+            {
+                IdentityId = identityId,
+                BasicInfo = basicInfo,
+                Created = DateTime.UtcNow,
+                LastUpdated = DateTime.UtcNow,
+            };
+            return UserProfile;
+        }
+        public void UpdateBasicInfo(BasicInfo basicInfo)
+        {
+            BasicInfo = basicInfo;
+            LastUpdated = DateTime.UtcNow;
+        }
+    }
+}
