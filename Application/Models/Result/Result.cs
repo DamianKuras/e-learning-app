c3018ee1b87754189f837479bdfab300764042e6
DamianKuras <damian_kuras@outlook.com>
2023-02-06 22:35:49 +0100

    refactor: improve and refactor result class

---
+++ Application/Models/Result/Result.cs
@@ -0,0 +1,36 @@
+﻿namespace Application.Models.Result
+{
+    public class Result<T>
+    {
+        public List<String> Errors { get; set; } = new List<String>();
+
+        public bool IsForbidden => Status == Status.Forbidden;
+        public bool IsInvalid => Status == Status.Invalid;
+
+        public bool IsNotFound => Status == Status.NotFound;
+        public bool IsSucces => Status == Status.Succes;
+        public bool IsUnauthorized => Status == Status.Unauthorized;
+        public T Payload { get; set; }
+        public Status Status { get; set; }
+
+        public void SetAsForbidden()
+        {
+            Status = Status.Forbidden;
+        }
+
+        public void SetAsInvalid()
+        {
+            Status = Status.Invalid;
+        }
+
+        public void SetAsNotFound()
+        {
+            Status = Status.NotFound;
+        }
+
+        public void SetAsUnauthorized()
+        {
+            Status = Status.Unauthorized;
+        }
+    }
+}
