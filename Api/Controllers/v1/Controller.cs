8b9e0c0c82e26bf33099b5081d88b6f0c93e26cd
DamianKuras <damian_kuras@outlook.com>
2023-02-06 22:37:43 +0100

    refactor: add base controller class and refactor user controller

---
+++ Api/Controllers/v1/Controller.cs
@@ -0,0 +1,37 @@
+﻿using Application.Models.Result;
+using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers.v1
+{
+    [ApiVersion("1.0")]
+    [ApiController]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [Produces("application/json")]
+    public class Controller : ControllerBase
+    {
+        private IMapper _mapperInstance;
+        private IMediator _mediatorInstance;
+        protected IMapper _mapper => _mapperInstance ??= HttpContext.RequestServices.GetService<IMapper>();
+        protected IMediator _mediator => _mediatorInstance ??= HttpContext.RequestServices.GetService<IMediator>();
+
+        protected IActionResult HandleErrors(Status status, List<string> Errors)
+
+        {
+            if (status == Status.NotFound)
+            {
+                return NotFound(Errors);
+            }
+            if (status == Status.Forbidden)
+            {
+                return Forbid();
+            }
+            if (status == Status.Unauthorized)
+            {
+                return Unauthorized(Errors);
+            }
+            return BadRequest(Errors);
+        }
+    }
+}
--- Api/Controllers/v1/UserController.cs
+++ Api/Controllers/v1/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using Api.Contracts.User;
 using Application.Enums;
+using Application.Models.Result;
 using Application.User.Commands;
 using AutoMapper;
 using MediatR;
@@ -7,33 +8,17 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers.v1
 {
-    [ApiController]
-    [ApiVersion("1.0")]
-    [Route("api/v{version:apiVersion}/[controller]")]
-    [Produces("application/json")]
     public class UserController : Controller
     {
-        private readonly IMapper _mapper;
-        private readonly IMediator _mediator;
-
-        public UserController(IMediator mediator, IMapper mapper)
-        {
-            _mediator = mediator;
-            _mapper = mapper;
-        }
-
         [HttpPost]
         [Route("/login")]
         public async Task<IActionResult> Login([FromBody] Login login)
         {
             var command = _mapper.Map<LoginUser>(login);
             var result = await _mediator.Send(command);
-            if (result.IsError)
+            if (!result.IsSucces)
             {
-                if (result.ErrorType == ErrorType.Unauthorized)
-                {
-                    return Unauthorized(result.Errors);
-                }
+                HandleErrors(result.Status, result.Errors);
             }
             return Ok(_mapper.Map<User>(result.Payload));
         }
@@ -44,16 +29,9 @@ namespace Api.Controllers.v1
         {
             var command = _mapper.Map<RegisterUser>(registration);
             var result = await _mediator.Send(command);
-            if (result.IsError)
+            if (!result.IsSucces)
             {
-                if (result.ErrorType == ErrorType.BadRequest)
-                {
-                    return BadRequest(result.Errors);
-                }
-                if (result.ErrorType == ErrorType.InternalServerError)
-                {
-                    return StatusCode(500, result.Errors);
-                }
+                HandleErrors(result.Status, result.Errors);
             }
             return Accepted();
         }
